6c3c460a2cf7ea46e52584aed42b922f32315b02
a8vk <a8vk@ya.ru>
2023-03-26 10:17:57 +1000

    Structuring the base

--- mc_donalds/models.py
+++ mc_donalds/models.py
@@ -1,24 +1,44 @@
 from django.db import models
 
+
 class Order(models.Model):
-    pass
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    take_away = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey("Staff", on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product", through='ProductOrder')
 
 
-# CREATE TABLE PRODUCTS (
-#     product_id INT AUTO_INCREMENT NOT NULL,
-#     name CHAR(255) NOT NULL,
-#     price FLOAT NOT NULL,
-#
-#     PRIMARY KEY (product_id)
-# );
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
+    # composition = models.TextField(default='Состав не указан')
+
+
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, 'Директор'),
+    (admin, 'Администратор'),
+    (cook, 'Повар'),
+    (cashier, 'Кассир'),
+    (cleaner, 'Уборщик')
+]
 
 
 class Staff(models.Model):
-    pass
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
+    labor_contract = models.IntegerField()
 
 
 class ProductOrder(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
